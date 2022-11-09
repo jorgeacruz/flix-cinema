f82d53daa52911e58bb24257fec46f22b6556c5a
Jorge.acruz <jorge.cruz@crmservives.com.br>
2022-11-09 10:39:56 -0300

    list Movies - part I

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,20 +9,30 @@ function Home() {
 
         async function loadFilmes() {
             const response = await api.get("movie/now_playing", {
-                params:{
-                    api_key:"91e6d8f32b0345ff9d247af3d3d08f31",
-                    language:"pt-BR",
-                    page:1
+                params: {
+                    api_key: "91e6d8f32b0345ff9d247af3d3d08f31",
+                    language: "pt-BR",
+                    page: 1
                 }
             })
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10));
         }
         loadFilmes();
     }, [])
 
     return (
-        <div>
-            <h2>Home</h2>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((f) => {
+                    return (
+                        <article key={f.id}>
+                            <strong>{f.title}</strong>
+                        </article>
+                    )
+                })}
+            </div>
+
         </div>
     )
 }
