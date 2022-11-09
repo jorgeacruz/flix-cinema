7ca99cf6b5e004e9dc5823f68f5ae14942152bc2
Jorge.acruz <jorge.cruz@crmservives.com.br>
2022-11-09 10:54:05 -0300

    List Movies - part I

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,47 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home() {
+    const [filmes, setFilmes] = useState([]);
 
-    const [filmes, setFilmes] = useState();
 
     useEffect(() => {
 
         async function loadFilmes() {
             const response = await api.get("movie/now_playing", {
                 params: {
-                    api_key: "91e6d8f32b0345ff9d247af3d3d08f31",
+                    api_key: "28fc232cc001c31e8a031f419d0a14ca",
                     language: "pt-BR",
-                    page: 1
+                    page: 1,
                 }
             })
             //console.log(response.data.results.slice(0, 10));
-            setFilmes(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10))
         }
+
         loadFilmes();
+
     }, [])
 
     return (
-        <div className='container'>
-            <div className='lista-filmes'>
+        <div className="container">
+            <div className="lista-filmes">
                 {filmes.map((f) => {
                     return (
                         <article key={f.id}>
                             <strong>{f.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${f.poster_path}`} alt={f.title} />
+                            <Link to={`/filme/${f.id}`}>Acessar</Link>
                         </article>
                     )
                 })}
             </div>
-
         </div>
     )
 }
+
 export default Home;
